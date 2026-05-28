895c72a06fa833b0a0c7ef2c5c58379c1296a1aa
Duy Thoại <54527891+TRANDUYTHOAI14@users.noreply.github.com>
2026-05-28 21:46:09 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -315,7 +315,7 @@ export default function TroThinhAuVietLandingPage() {
                   Gọi Tư Vấn Ngay
                   <ArrowRight className="h-4 w-4 transition-transform group-hover:translate-x-1" />
                 </CTAButton>
-                <CTAButton href="#services" variant="secondary">
+                <CTAButton href="https://trothinhauviet.com/dich-vu-tro-thinh-au-viet/" variant="secondary">
                   <Volume2 className="h-5 w-5" />
                   Xem Dịch Vụ
                 </CTAButton>
